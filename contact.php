1c43ba571643fa48b7151e86bd8a5f9b0f202910
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-11 05:42:09 -0500

    contact fix

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Contact | Official Squirt Site!</title>
-		
 		<link rel="icon" href="/images/favicon.png" type="image/x-icon">		
 		<link rel="stylesheet" href="/assets/css/style.css" type="text/css">			
 		<meta name="Keywords" content="squirt, squirtpunk, squirtmusic, music, pop, punk, rock, bass, guitar, drums, newport news, virginia, hampton roads, warped, tour, headache, heartache, dorky, kids, discography, shows, spotify, apple, news, bios">
@@ -10,22 +10,23 @@
 		<meta property="og:description" content="Squirt is a small group of dorky kids from Newport News, VA playing pop-punk music circa the year 2000.">
 		<meta property="og:image:type" content="image/jpeg">
 		<meta property="og:image" content="/images/squirt-group.jpg">
-		<meta property="og:url" content="">		<meta name="Description" content="Go back in the past and contact the members of Squirt as they were circa the year 2000.">					
+		<meta property="og:url" content="">		
+		<meta name="Description" content="Go back in the past and contact the members of Squirt as they were circa the year 2000.">
+	</head>
 	
-		
+	<body>		
 		<a href="/"><img src="/images/logo.jpg" width=""></a>
 
-	
+		<div id="primary-navigation" class="text-right">
+			<a class="" href="/shows.php">Shows</a> 
+			<a class="" href="/bios.php">Bios</a> 
+			<a class="" href="/news.php">News</a> 
+			<a class="" href="/guestbook.php">Guestbook</a> 
+			<a class="" href="/discography.php">Discography</a> 
+			<a class="" href="/faqs.php">FAQs</a> 
+			<a class="active" href="/contact.php">Contact</a> 
+		</div>
 
-<div id="primary-navigation" class="text-right">
-	<a class="" href="/shows.php">Shows</a> 
-	<a class="" href="/bios.php">Bios</a> 
-	<a class="" href="/news.php">News</a> 
-	<a class="" href="/guestbook.php">Guestbook</a> 
-	<a class="" href="/discography.php">Discography</a> 
-	<a class="" href="/faqs.php">FAQs</a> 
-	<a class="active" href="/contact.php">Contact</a> 
-</div>				
 		<div class="center">
 			<h1>Contact</h1>
 			<p>Contact options <b style="color:#000;background:#ffff66">coming</b> <b style="color:#000;background:#66ffff">soon</b>.</p>	
@@ -35,5 +36,6 @@
 		<footer class="text-center">
 			<hr color="#424242">
 			<p class="text-center standard gray">© 2000-2022 Squirt | Site originally designed and lovingly restored by <a class="standard gray underline" href="https://htmyell.com/" target="_blank">Htmyell Design &amp; Development</a></p>
-		</footer>		
-</div></body></html>
+		</footer>
+	</body>
+</html>
