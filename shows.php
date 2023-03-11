65f28c2f3e6a1ff2ef1f1f1b8ecdc08eeb4281ce
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-11 05:53:51 -0500

    shows

--- shows.php
+++ shows.php
@@ -1,7 +1,7 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Shows | Official Squirt Site!</title>
-		
 		<link rel="icon" href="/images/favicon.png" type="image/x-icon">		
 		<link rel="stylesheet" href="/assets/css/style.css" type="text/css">			
 		<meta name="Keywords" content="squirt, squirtpunk, squirtmusic, music, pop, punk, rock, bass, guitar, drums, newport news, virginia, hampton roads, warped, tour, headache, heartache, dorky, kids, discography, shows, spotify, apple, news, bios">
@@ -10,24 +10,25 @@
 		<meta property="og:description" content="Squirt is a small group of dorky kids from Newport News, VA playing pop-punk music circa the year 2000.">
 		<meta property="og:image:type" content="image/jpeg">
 		<meta property="og:image" content="/images/squirt-group.jpg">
-		<meta property="og:url" content="">		<meta name="Description" content="Shows that Squirt played with other bands in and around Hampton Roads circa the year 2000.">		
-	
-		
+		<meta property="og:url" content="">		
+		<meta name="Description" content="Shows that Squirt played with other bands in and around Hampton Roads circa the year 2000.">		
+	</head>
+	<body>	
 		<a href="/"><img src="/images/logo.jpg" width=""></a>
 
-	
+		<div id="primary-navigation" class="text-right">
+			<a class="active" href="/shows.php">Shows</a> 
+			<a class="" href="/bios.php">Bios</a> 
+			<a class="" href="/news.php">News</a> 
+			<a class="" href="/guestbook.php">Guestbook</a> 
+			<a class="" href="/discography.php">Discography</a> 
+			<a class="" href="/faqs.php">FAQs</a> 
+			<a class="" href="/contact.php">Contact</a> 
+		</div>
 
-<div id="primary-navigation" class="text-right">
-	<a class="active" href="/shows.php"><b style="color:#000;background:#ffff66">Shows</b></a> 
-	<a class="" href="/bios.php">Bios</a> 
-	<a class="" href="/news.php">News</a> 
-	<a class="" href="/guestbook.php">Guestbook</a> 
-	<a class="" href="/discography.php">Discography</a> 
-	<a class="" href="/faqs.php">FAQs</a> 
-	<a class="" href="/contact.php">Contact</a> 
-</div>			
-		<h1 class="text-center">Upcoming <b style="color:#000;background:#ffff66">Shows</b></h1>
-		<p class="text-center">This is the list of <b style="color:#000;background:#ffff66">shows</b> past and future make sure you the day 2 before the <b style="color:#000;background:#ffff66">shows</b> to make sure they are still set.</p>		
+		<h1 class="text-center">Upcoming Shows</h1>
+		<p class="text-center">This is the list of shows past and future make sure you the day 2 before the shows to make sure they are still set.</p>		
+		
 		<table cellpadding="0" cellspacing="0" align="center" width="80%">
 			<tbody>
 				<tr valign="top">
@@ -35,12 +36,12 @@
 						<table border="1" bordercolor="Black" cellpadding="0" cellspacing="0" width="100%">
 							<tbody>
 								<tr> 
-									<td bgcolor="Gray"><b><font color="black"><b style="color:#000;background:#ffff66">SHOWS</b>!!!!!!</font></b></td>
+									<td bgcolor="Gray"><b><font color="black">SHOWS!!!!!!</font></td>
 								</tr>
 								<tr> 
 									<td bgcolor="White">
 										<font color="black">
-											<div align="center">June 17th at Cogan's @7pm Squirt w/2 other local bands $5<br><br>June 18th at the Underground @8pm Squirt w/The No Talent Show and Oh86 $5<br><br>June 22nd at Cogan's @7pm Squirt w/SHINDIG, Backside Slappy, Heaven and Breakfast and Punchline $5<br><br>July 8th at the Underground @8pm Squirt w/Clearly Labeled and Striving $5 <br><br>July 16th at the Underground @8pm Squirt w/ Ten Years From Now, the Satellites, and ...And More $5<br><br>July 28th<br>All your favorite local bands are playing:<br>Stage 1:<br>/////////////////////////////////////////////////////////<br>12pm - plastic toy guns<br>1pm - ruckus<br>2pm - dead hate the living<br>3pm - oh86<br>4pm - anti violet<br>5pm - kill your crush<br>6pm - shindig<br>7pm - the sattelites <br>8pm - bailout<br>9pm - project 208<br>10pm - 65 film show<br><br>Stage 2<br>////////////////////////////////////////////////////////<br><br>12:30pm - and more<br>1:30pm - clockin neemin<br>2:30pm - flex friday<br>3:30pm - clearly labeled<br>4:30pm - the no talent show<br>5:30pm - jet by day (?)<br>6:30pm - ten years from now<br>7:30pm - rst video<br>8:30pm - SQUIRT<br>9:30pm - backside slappy<br>10:30pm - peg 75<br><br>August 14th at Cogan's @7pm Squirt w/ AGENT FELIX and Unsung Zeros $5 <br><br>MORE <b style="color:#000;background:#ffff66">SHOWS</b> TO COME!<br>
+											<div align="center">June 17th at Cogan's @7pm Squirt w/2 other local bands $5<br><br>June 18th at the Underground @8pm Squirt w/The No Talent Show and Oh86 $5<br><br>June 22nd at Cogan's @7pm Squirt w/SHINDIG, Backside Slappy, Heaven and Breakfast and Punchline $5<br><br>July 8th at the Underground @8pm Squirt w/Clearly Labeled and Striving $5 <br><br>July 16th at the Underground @8pm Squirt w/ Ten Years From Now, the Satellites, and ...And More $5<br><br>July 28th<br>All your favorite local bands are playing:<br>Stage 1:<br>/////////////////////////////////////////////////////////<br>12pm - plastic toy guns<br>1pm - ruckus<br>2pm - dead hate the living<br>3pm - oh86<br>4pm - anti violet<br>5pm - kill your crush<br>6pm - shindig<br>7pm - the sattelites <br>8pm - bailout<br>9pm - project 208<br>10pm - 65 film show<br><br>Stage 2<br>////////////////////////////////////////////////////////<br><br>12:30pm - and more<br>1:30pm - clockin neemin<br>2:30pm - flex friday<br>3:30pm - clearly labeled<br>4:30pm - the no talent show<br>5:30pm - jet by day (?)<br>6:30pm - ten years from now<br>7:30pm - rst video<br>8:30pm - SQUIRT<br>9:30pm - backside slappy<br>10:30pm - peg 75<br><br>August 14th at Cogan's @7pm Squirt w/ AGENT FELIX and Unsung Zeros $5 <br><br>MORE SHOWS TO COME!<br>
 											</div>
 										</font>
 									</td>
@@ -58,7 +59,7 @@
 									<td>
 										<div class="black">
 											Join the Mailing List for constant<br> 
-											up to date e-mails about our <b style="color:#000;background:#ffff66">shows</b> <br>
+											up to date e-mails about our shows <br>
 											and cancelations (assuming the clubs<br> 
 											are that stupid haha)
 											<p class="text-right">-Jamie</p>
@@ -70,10 +71,11 @@
 					</td>
 				</tr>
 			</tbody>
-	</table>
+		</table>
 
 		<footer class="text-center">
 			<hr color="#424242">
 			<p class="text-center standard gray">© 2000-2022 Squirt | Site originally designed and lovingly restored by <a class="standard gray underline" href="https://htmyell.com/" target="_blank">Htmyell Design &amp; Development</a></p>
 		</footer>	
-</div></body></html>
+	</body>
+</html>
