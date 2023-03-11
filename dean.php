0c597b58562bedc3ee74878cf1c5832988ae1c5e
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-11 07:07:26 -0500

    bio fixes

--- dean.php
+++ dean.php
@@ -1,7 +1,7 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Dean | Official Squirt Site!</title>
-		
 		<link rel="icon" href="/images/favicon.png" type="image/x-icon">		
 		<link rel="stylesheet" href="/assets/css/style.css" type="text/css">			
 		<meta name="Keywords" content="squirt, squirtpunk, squirtmusic, music, pop, punk, rock, bass, guitar, drums, newport news, virginia, hampton roads, warped, tour, headache, heartache, dorky, kids, discography, shows, spotify, apple, news, bios">
@@ -10,22 +10,25 @@
 		<meta property="og:description" content="Squirt is a small group of dorky kids from Newport News, VA playing pop-punk music circa the year 2000.">
 		<meta property="og:image:type" content="image/jpeg">
 		<meta property="og:image" content="/images/squirt-group.jpg">
-		<meta property="og:url" content="">		<meta name="Description" content="Dean Drumwright, Singer, Songwriter, Guitarer circa the year 2000.">
+		<meta property="og:url" content="">
+		<meta name="Description" content="Dean Drumwright, Singer, Songwriter, Guitarer circa the year 2000.">
+	</head>
+	<body>	
 	
-		
-		<a href="/"><img src="/images/logo.jpg" width=""></a>
+		<a href="/"><img src="/images/logo.jpg"></a>
 
+		<div id="primary-navigation" class="text-right">
+			<a class="" href="/shows.php">Shows</a> 
+			<a class="active" href="/bios.php">Bios</a> 
+			<a class="" href="/news.php">News</a> 
+			<a class="" href="/guestbook.php">Guestbook</a> 
+			<a class="" href="/discography.php">Discography</a> 
+			<a class="" href="/faqs.php">FAQs</a> 
+			<a class="" href="/contact.php">Contact</a> 
+		</div>	
 
-<div id="primary-navigation" class="text-right">
-	<a class="" href="/shows.php">Shows</a> 
-	<a class="active" href="/bios.php">Bios</a> 
-	<a class="" href="/news.php">News</a> 
-	<a class="" href="/guestbook.php">Guestbook</a> 
-	<a class="" href="/discography.php">Discography</a> 
-	<a class="" href="/faqs.php">FAQs</a> 
-	<a class="" href="/contact.php">Contact</a> 
-</div>	
 		<h1 class="text-center">Dean Drumwright</h1>					
+		
 		<div class="center">
 			<img src="/images/deanbioback.jpg" width="675" height="290"><br><br>
 			<a class="block" href="/bios.php">&lt;&lt; Back</a>
@@ -34,5 +37,7 @@
 		<footer class="text-center">
 			<hr color="#424242">
 			<p class="text-center standard gray">© 2000-2022 Squirt | Site originally designed and lovingly restored by <a class="standard gray underline" href="https://htmyell.com/" target="_blank">Htmyell Design &amp; Development</a></p>
-		</footer>		
-</div></body></html>
+		</footer>
+
+	</body>
+</html>
--- jamie.php
+++ jamie.php
@@ -1,31 +1,34 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Jamie | Official Squirt Site!</title>
-		
 		<link rel="icon" href="/images/favicon.png" type="image/x-icon">		
-		<link rel="stylesheet" href="./jamie_files/style.css" type="text/css">			
+		<link rel="stylesheet" href="/assets/css/style.css" type="text/css">			
 		<meta name="Keywords" content="squirt, squirtpunk, squirtmusic, music, pop, punk, rock, bass, guitar, drums, newport news, virginia, hampton roads, warped, tour, headache, heartache, dorky, kids, discography, shows, spotify, apple, news, bios">
 		<meta property="og:type" content="website">	
 		<meta property="og:title" content="Official Squirt Site!">
 		<meta property="og:description" content="Squirt is a small group of dorky kids from Newport News, VA playing pop-punk music circa the year 2000.">
 		<meta property="og:image:type" content="image/jpeg">
 		<meta property="og:image" content="/images/squirt-group.jpg">
-		<meta property="og:url" content="">		<meta name="Description" content="Jamie Taylor, Drummist circa the year 2000.">		
+		<meta property="og:url" content="">		
+		<meta name="Description" content="Jamie Taylor, Drummist circa the year 2000.">		
+	</head>	
+	<body>
+
+		<a href="/"><img src="./jamie_files/logo.jpg"></a>	
+
+		<div id="primary-navigation" class="text-right">
+			<a class="" href="/shows.php">Shows</a> 
+			<a class="active" href="/bios.php">Bios</a> 
+			<a class="" href="/news.php">News</a> 
+			<a class="" href="/guestbook.php">Guestbook</a> 
+			<a class="" href="/discography.php">Discography</a> 
+			<a class="" href="/faqs.php">FAQs</a> 
+			<a class="" href="/contact.php">Contact</a> 
+		</div>		
 	
+		<h1 class="text-center">Jamie Taylor</h1>		
 		
-		<a href="/"><img src="./jamie_files/logo.jpg" width=""></a>
-
-	
-
-<div id="primary-navigation" class="text-right">
-	<a class="" href="/shows.php">Shows</a> 
-	<a class="active" href="/bios.php">Bios</a> 
-	<a class="" href="/news.php">News</a> 
-	<a class="" href="/guestbook.php">Guestbook</a> 
-	<a class="" href="/discography.php">Discography</a> 
-	<a class="" href="/faqs.php">FAQs</a> 
-	<a class="" href="/contact.php">Contact</a> 
-</div>		<h1 class="text-center"><b style="color:#000;background:#ffff66">Jamie</b> Taylor</h1>		
 		<div class="center">
 			<img src="/images/jamiebioback.jpg" width="675" height="290">
 			<a class="block" href="/bios.php">&lt;&lt; Back</a>
@@ -35,4 +38,5 @@
 			<hr color="#424242">
 			<p class="text-center standard gray">© 2000-2022 Squirt | Site originally designed and lovingly restored by <a class="standard gray underline" href="https://htmyell.com/" target="_blank">Htmyell Design &amp; Development</a></p>
 		</footer>		
-</div></body></html>
+	</body>
+</html>
