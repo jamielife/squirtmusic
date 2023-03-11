5518bf5291836251228772c918f237957c1ecfb0
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-11 06:12:20 -0500

    nav size

--- discography.php
+++ discography.php
@@ -1,7 +1,7 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Discography | Official Squirt Site!</title>
-		
 		<link rel="icon" href="/images/favicon.png" type="image/x-icon">		
 		<link rel="stylesheet" href="/assets/css/style.css" type="text/css">			
 		<meta name="Keywords" content="squirt, squirtpunk, squirtmusic, music, pop, punk, rock, bass, guitar, drums, newport news, virginia, hampton roads, warped, tour, headache, heartache, dorky, kids, discography, shows, spotify, apple, news, bios">
@@ -11,21 +11,20 @@
 		<meta property="og:image:type" content="image/jpeg">
 		<meta property="og:image" content="/images/squirt-group.jpg">
 		<meta property="og:url" content="">		<meta name="Description" content="Discographuy of demos, EPs, and albums Squirt recorded circa the year 2000.">				
-	
-	
+	</head>
+	<body>
 		<a href="/"><img src="/images/logo.jpg" width=""></a>
 
-	
+		<div id="primary-navigation" class="text-right">
+			<a class="" href="/shows.php">Shows</a> 
+			<a class="" href="/bios.php">Bios</a> 
+			<a class="" href="/news.php">News</a> 
+			<a class="" href="/guestbook.php">Guestbook</a> 
+			<a class="active" href="/discography.php">Discography</a> 
+			<a class="" href="/faqs.php">FAQs</a> 
+			<a class="" href="/contact.php">Contact</a> 
+		</div>		
 
-<div id="primary-navigation" class="text-right">
-	<a class="" href="/shows.php">Shows</a> 
-	<a class="" href="/bios.php">Bios</a> 
-	<a class="" href="/news.php">News</a> 
-	<a class="" href="/guestbook.php">Guestbook</a> 
-	<a class="active" href="/discography.php">Discography</a> 
-	<a class="" href="/faqs.php">FAQs</a> 
-	<a class="" href="/contact.php">Contact</a> 
-</div>			
 		<center>
 			<h1>Discography</h1>
 			<h2>Select an album:</h2>
@@ -36,7 +35,7 @@
 							<a href="/discography.php#" title="We Suck but... hey you only paid a dollar" onclick="alert(&#39;This was a cassette, if you know what that is. It has been lost to time.&#39;);">
 								<img src="/images/disco-we-suck-but-you-only-paid-a-dollar.jpg" alt="we suck, but hey you only paid a dollar demo" border="0">
 								<br>
-								<span class="small"><b style="color:#000;background:#ffff66">We</b> <b style="color:#000;background:#66ffff">Suck</b> But...</span>
+								<span class="small">>We Suck But...</span>
 								<br>
 								<span class="small">Demo</span>
 							</a> 
@@ -70,4 +69,5 @@
 			<p class="text-center standard gray">© 2000-2022 Squirt | Site originally designed and lovingly restored by <a class="standard gray underline" href="https://htmyell.com/" target="_blank">Htmyell Design &amp; Development</a></p>
 		</footer>			
 	
-	</div></body></html>
+	</body>
+</html>
