638e2508787cb501afbdee822151356b1da8c863
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-13 09:24:22 -0400

    ...2

--- components/nav.php
+++ components/nav.php
@@ -4,7 +4,7 @@
 			<a <?php if($_SERVER['PHP_SELF'] == '/news.php'): ?> class="active"<?php endif; ?> href="/news.php">News</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/guestbook.php'): ?> class="active"<?php endif; ?> href="/guestbook.php">Guestbook</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/discography.php'): ?> class="active"<?php endif; ?> href="/discography.php">Discography</a> 
-			<a <?php if($_SERVER['PHP_SELF'] == '/showsfaqs.php'): ?> class="active"<?php endif; ?> href="/faqs.php">FAQs</a> 
+			<a <?php if($_SERVER['PHP_SELF'] == '/faqs.php'): ?> class="active"<?php endif; ?> href="/faqs.php">FAQs</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/contact.php'): ?> class="active"<?php endif; ?> href="/contact.php">Contact</a> 
 		</div>		
 			<?php echo $_SERVER['PHP_SELF']; ?>
