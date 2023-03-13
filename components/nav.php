2d58aa41068082d5b1abcd5a7add1aa04172ec16
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-13 09:39:28 -0400

    test

--- components/nav.php
+++ components/nav.php
@@ -2,9 +2,8 @@
 			<a <?php if($_SERVER['PHP_SELF'] == '/shows.php'): ?> class="active"<?php endif; ?> href="/shows.php">Shows</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/bios.php'): ?> class="active"<?php endif; ?> href="/bios.php">Bios</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/news.php'): ?> class="active"<?php endif; ?> href="/news.php">News</a> 
-			<a <?php if($_SERVER['PHP_SELF'] == '/guestbook.php'): ?> class="active"<?php endif; ?> href="/guestbook.php">Guestbook</a> 
+			<a <?php if(in_array($_SERVER['PHP_SELF'], array('/guestbook.php', '/jamie.php'))): ?> class="active"<?php endif; ?> href="/guestbook.php">Guestbook</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/discography.php'): ?> class="active"<?php endif; ?> href="/discography.php">Discography</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/faqs.php'): ?> class="active"<?php endif; ?> href="/faqs.php">FAQs</a> 
 			<a <?php if($_SERVER['PHP_SELF'] == '/contact.php'): ?> class="active"<?php endif; ?> href="/contact.php">Contact</a> 
 		</div>		
-        
