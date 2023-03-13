41200596ac42aba7c6e9a0a152b954bdfa244e0c
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-13 10:17:03 -0400

    revert

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,4 @@
 <footer class="text-center" <?php if($_SERVER['PHP_SELF'] == '/index.php'): ?>style="position: absolute; top: 400px; display: block; width: 100%"<?php endif; ?>>
 			<hr color="#424242" />
-			<p class="text-center standard gray" >&copy; 2000-<?php echo date('Y'); ?> Squirt | Site originally designed and lovingly restored by... <a class="standard gray underline" href="https://htmyell.com" target="_blank">Htmyell Design & Development</a></p>
+			<p class="text-center standard gray" >&copy; 2000-<?php echo date('Y'); ?> Squirt | Site originally designed and lovingly restored by <a class="standard gray underline" href="https://htmyell.com" target="_blank">Htmyell Design & Development</a></p>
 		</footer>	
